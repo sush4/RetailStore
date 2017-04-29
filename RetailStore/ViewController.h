5bf91f13e4bbc6f4482fedfdf29fb33a16093b1e
Sushant kumar <sush.nic@gmail.com>
2017-04-30 02:09:12 +0530

    Init repo.
    Init repo.

---
+++ RetailStore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RetailStore
+//
+//  Created by Sushant kumar on 30/04/17.
+//  Copyright © 2017 Sushant kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RetailStore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RetailStore
+//
+//  Created by Sushant kumar on 30/04/17.
+//  Copyright © 2017 Sushant kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
